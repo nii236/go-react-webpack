b6a306b0c5d88a5934e52de7e0999253138b5a97
John Nguyen <jtnguyen236@gmail.com>
2016-02-03 12:07:33 +0800

    fixed CORS requests from react app

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/goadesign/goa"
 	"github.com/goadesign/middleware"
+	"github.com/goadesign/middleware/cors"
 	"github.com/nii236/go-react-webpack/app"
 	"github.com/nii236/go-react-webpack/js"
 	"github.com/nii236/go-react-webpack/swagger"
@@ -16,6 +17,23 @@ func main() {
 	service.Use(middleware.RequestID())
 	service.Use(middleware.LogRequest())
 	service.Use(middleware.Recover())
+	cspec, err := cors.New(func() {
+		cors.Origin("*", func() {
+			cors.Resource("/add/*", func() {
+				cors.Headers("Accept", "Content-Type", "Origin", "Authorization")
+				cors.Methods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				cors.MaxAge(600)
+				cors.Credentials(true)
+				cors.Vary("Http-Origin")
+			})
+		})
+	})
+	if err != nil {
+		panic(err)
+	}
+	// mount the cors controller
+	service.Use(cors.Middleware(cspec))
+	cors.MountPreflightController(service, cspec)
 
 	// Mount "operands" controller
 	c := NewOperandsController(service)
